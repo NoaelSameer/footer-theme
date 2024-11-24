7c7222e0e81fb5343183ca791714d47d6810d537
NoaelSameer <142361729+NoaelSameer@users.noreply.github.com>
2024-11-23 23:30:20 -0700

    h

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,13 +1,31 @@
+import Footer from "../components/Footer_Theme";
 
-import Footer from "../components/Footer_Theme"
+function FooterTest() {
+    const logo = "https://cdn.pixabay.com/photo/2022/08/22/02/05/logo-7402513_640.png";
+    const about = "© 2024 The Bobbingtons. All rights reserved.";
+    const socialLinks = {
+        github: "https://github.com",
+        instagram: "https://instagram.com",
+        facebook: "https://facebook.com",
+        reddit: "https://reddit.com"
+    };
+    const services = [
+        { name: "Affiliates", href: "/affiliates" },
+        { name: "Contact Us", href: "/contact" },
+        { name: "Support", href: "/support" },
+        { name: "Services", href: "/services" }
+    ];
 
-
-function FooterTest(){
-    return(
-    <>
-    <Footer about="jeu"/>
-    </>
-    )
+    return (
+        <>
+            <Footer
+                logo={logo}
+                about={about}
+                socialLinks={socialLinks}
+                services={services}
+            />
+        </>
+    );
 }
 
 export default FooterTest;
